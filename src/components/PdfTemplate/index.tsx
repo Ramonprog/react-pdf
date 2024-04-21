ef5848a555be4e9558e84442ae7a027ec730d863
Ramonprog <programador.ramon@gmail.com>
2024-04-21 12:47:28 -0300

    feat: modelo inicial pdf

--- src/components/PdfTemplate/index.tsx
+++ src/components/PdfTemplate/index.tsx
@@ -1,7 +1,29 @@
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+
 export function PdfTemplate() {
+
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#E4E4E4',
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+  });
+
   return (
-    <div>
-      <h1>PDF Template</h1>
-    </div>
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>Section #1</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
   );
 }
